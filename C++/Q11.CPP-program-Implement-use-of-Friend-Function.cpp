6541713632f3db5c6a36e481bc08db4f3401aa90
Sonu Kumar <81131231+Sonu31@users.noreply.github.com>
2021-08-24 12:14:59 +0530

    Create Q11.CPP-program-Implement-use-of-Friend-Function.cpp

---
+++ C++/Q11.CPP-program-Implement-use-of-Friend-Function.cpp
@@ -0,0 +1,20 @@
+class sample{
+   int length, breadth;
+ 
+   public://cons
+   sample(int length, int breadth):length(length),breadth(breadth)
+   {}
+   friend void calcArea(sample s); //friend function dec
+ 
+};
+//friend function def
+void calcArea(sample s){
+   cout<<"Area = "<<s.length * s.breadth;
+   }
+int main()
+   {
+      sample s(10,15);
+      calcArea(s);
+ 
+      return 0;
+}
